927ed61e250ecc3f56bddb0a41d74d1c67cfb94c
leonardofl <leonardofl87@gmail.com>
2013-04-15 22:56:43 -0300

    debuger que lista quais turmas estão em quais arquivos

---
+++ src/sp/alvaro/odf/NullProgressListener.java
@@ -0,0 +1,16 @@
+package sp.alvaro.odf;
+
+/**
+ * Usa o design pattern Null Object
+ * @author leonardo
+ *
+ */
+public class NullProgressListener implements ProgressListener {
+
+	@Override
+	public void progress(int progress) {
+
+		// faz nada :-)
+	}
+
+}
